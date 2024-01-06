adf7824496761b36e6eec8fdb1c16fc951a80628
Your Name <Yanzick>
2024-01-06 11:28:05 +0700

    finished project

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("com.android.library") version "8.1.2" apply false
+}
+/*buildscript {
+    repositories {
+        google()
+        jcenter()
+        mavenCentral()
+        maven {
+            url = uri("https://repo.eclipse.org/content/repositories/paho-releases/")
+        }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.2")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+tasks.register("clean") {
+    doLast {
+        delete(rootProject.buildDir)
+    }
+}*/
